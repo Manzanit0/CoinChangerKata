5c8cc06e0c8296f91e1eecce378c809056b5d654
Javier Manzano <unico.javier@gmail.com>
2018-10-04 11:27:23 +0100

    2p returns 2p

--- lib/CoinChanger.rb
+++ lib/CoinChanger.rb
@@ -1,6 +1,7 @@
 class CoinChanger
 
   def self.change(amount)
+    return [2] if amount > 1
     [1]
   end
 end
--- spec/CoinChanger_spec.rb
+++ spec/CoinChanger_spec.rb
@@ -2,7 +2,11 @@
 require('CoinChanger')
 
 RSpec.describe CoinChanger do
-  it "1p" do
+  it "1p returns 1p" do
     expect(CoinChanger.change(1)).to eql([1])
+  end
+
+  it "2p returns 2p" do
+    expect(CoinChanger.change(2)).to eql([2])
   end
 end
